706db06a4aa9d3d06e48f9a30e0a3bd5c378e1de
Paul Sullivan <paul8sullivan@gmail.com>
2024-01-15 02:42:12 -0500

    models looking good

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,34 +1,29 @@
 const { Schema, Types } = require('mongoose');
 
-const assignmentSchema = new Schema(
+const reactionSchema = new Schema(
   {
-    assignmentId: {
+    reactionId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId(),
     },
-    assignmentName: {
+    reactionBody: {
       type: String,
       required: true,
-      maxlength: 50,
-      minlength: 4,
-      default: 'Unnamed assignment',
+      maxlength: 280,
     },
-    score: {
-      type: Number,
+    username: {
+      type: String,
       required: true,
-      default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
     },
     createdAt: {
       type: Date,
       default: Date.now,
     },
   },
-  {
-    toJSON: {
-      getters: true,
-    },
-    id: false,
-  }
 );
 
-module.exports = assignmentSchema;
+reactionSchema.path('createdAt').get(function(value) {
+  return new Date(value).toLocaleString();
+});
+
+module.exports = reactionSchema;
